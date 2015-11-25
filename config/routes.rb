a5b0e938cf51b743e8d2a15b7b50e58f402b5b57
mostafahosny <mostafa.hosny2015@hotmail.com>
2015-11-25 13:28:21 +0200

    added squire of fourir

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :experiments
+
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users
   root to: 'users#index'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116193311) do
+ActiveRecord::Schema.define(version: 20151125075928) do
+
+  create_table "experiments", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
