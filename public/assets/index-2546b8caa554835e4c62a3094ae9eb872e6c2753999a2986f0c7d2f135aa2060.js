2370689df05ec23a41eb7b833930897a6bdb1274
mostafahosny <mostafa.hosny2015@hotmail.com>
2015-12-03 16:08:33 +0200

    vendor compiled assets

---
+++ public/assets/index-2546b8caa554835e4c62a3094ae9eb872e6c2753999a2986f0c7d2f135aa2060.js
@@ -0,0 +1,1 @@
+var smoothie=new SmoothieChart;smoothie.streamTo(document.getElementById("mycanvas"));var line1=new TimeSeries,line2=new TimeSeries;setInterval(function(){line1.append((new Date).getTime(),.2),line2.append((new Date).getTime(),Math.random())},1e3),smoothie.addTimeSeries(line1),smoothie.addTimeSeries(line2);
