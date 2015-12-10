8325156473bddd5ac55fda39b10d9d96ae5eca9d
mostafahosny <mostafa.hosny2015@hotmail.com>
2015-12-10 15:07:43 +0200

    added theme

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users
-  root to: 'users#index'
+  root to: 'home#index'
+  resources :home
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
